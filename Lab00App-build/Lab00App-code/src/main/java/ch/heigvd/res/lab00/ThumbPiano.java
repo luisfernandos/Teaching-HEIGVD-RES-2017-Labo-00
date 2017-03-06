f16b8174b4e0c985b7e544cec62faef06ea66a35
wojciech_myszkorowski <wojciech.myszkorowski@heig-vd.ch>
2017-03-06 14:16:14 +0000

    Add implementation for ThumbPiano

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/ThumbPiano.java
@@ -0,0 +1,19 @@
+package ch.heigvd.res.lab00;
+
+interface IInstrument {
+
+	public String play();
+}
+
+public class ThumbPiano implements IInstrument {
+
+	private String sound;
+
+	public ThumbPiano() {
+		sound = "cling";
+	}
+
+	public String play() {
+		return sound;
+	}
+}
