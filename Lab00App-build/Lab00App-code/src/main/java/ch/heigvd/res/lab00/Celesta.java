828efb5fcb800683f200bfbcf8bf00a67910898a
JulienBrechet <julien.brechet@heig-vd.ch>
2017-03-06 12:46:27 +0100

    Celesta class implemented.

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Celesta.java
@@ -0,0 +1,14 @@
+package ch.heigvd.res.lab00;
+
+/**
+ * Created by Julien Brêchet on 06.03.2017.
+ */
+public class Celesta {
+    public String play() {
+        return "gling";
+    }
+    public int getSoundVolume() {
+        return 4;
+    }
+    public String getColor() { return "black"; }
+}
