7c4c6b9451b4b0a90c894ee2786b39ef492438ed
P-B Monaco <pierre-benjamin.monaco@heig-vd.ch>
2017-03-07 08:17:25 +0100

    Add implementation for Tabla

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Tabla.java
@@ -0,0 +1,11 @@
+package ch.heigvd.res.lab00;
+
+/**
+ * Classe implémentant un tabla
+ * @author P-B Monaco
+ */
+
+public class Tabla implements IInstrument
+{
+   public String play(){return "BoumBoum";}
+}
