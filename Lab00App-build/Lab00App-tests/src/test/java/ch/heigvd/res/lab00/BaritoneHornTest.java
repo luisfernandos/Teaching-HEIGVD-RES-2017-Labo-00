7ed9ff06075f5f647b763e0571ff8f46c6ad94a6
luca15 <luca.sivillica@hotmail.com>
2017-03-08 20:59:37 +0100

    Fixed Badoneon, Balalaika

---
+++ Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/BaritoneHornTest.java
@@ -0,0 +1,20 @@
+
+package ch.heigvd.res.lab00;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author annie
+ */
+public class BaritoneHornTest {
+   
+   @Test
+  public void aBaritoneHornShouldMakeConcertina() {
+    IInstrument baritoneHorn = new BaritoneHorn();
+    String sound = baritoneHorn.play();
+    Assert.assertEquals("concertina", sound);
+  }
+   
+}
