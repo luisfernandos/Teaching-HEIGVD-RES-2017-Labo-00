4ac2b3bbda35e344e2dba2c577b364376a1ecacb
Thuy-My <thuy-my.tran@epfl.ch>
2017-03-08 14:10:00 +0100

    Added implementation for bass guitar

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/BassGuitar.java
@@ -0,0 +1,8 @@
+package ch.heigvd.res.lab00;
+
+
+public class BassGuitar implements IInstrument {
+    public String bassGuitarShouldDoAWalk() {
+        return "Dum Tudum Tudum Tudum Dum Dum !";
+    }
+}
