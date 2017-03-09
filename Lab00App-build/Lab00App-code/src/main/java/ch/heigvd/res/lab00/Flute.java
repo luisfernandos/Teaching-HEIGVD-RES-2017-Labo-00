d8cf5075533930aabae5452459da1af0a475d057
doriane <doriane.tedongmokaffo@heig-vd.ch>
2017-03-09 16:00:23 +0100

    save

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
@@ -0,0 +1,22 @@
+package ch.heigvd.res.lab00;
+
+/**
+ *
+ * @author dorianekaffo
+ */
+public class Flute implements IInstrument{
+    @Override
+    public String play() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getSoundVolume() {
+        return 10;
+    }
+
+    @Override
+    public String getColor() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/IInstrument.java
@@ -0,0 +1,15 @@
+package ch.heigvd.res.lab00;
+
+/**
+ *
+ * @author dorianekaffo
+ */
+public interface IInstrument {
+    public String play();
+
+    public int getSoundVolume();
+
+    public String getColor();
+
+}
+
