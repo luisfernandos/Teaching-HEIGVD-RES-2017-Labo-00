89eefe72babdbb9987cbc6a8a1c1d057b7975769
doriane <doriane.tedongmokaffo@heig-vd.ch>
2017-03-15 08:13:44 +0100

    Fixes #415

--- Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Bassoon.java
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Bassoon.java
@@ -2,18 +2,23 @@ package ch.heigvd.res.lab00;
 
 /** 
 * @author David Truan
+* 
+* @author doriane kaffo 
 */
 
-class Bassoon implements IInstrument{
+public class Bassoon implements IInstrument{
+        @Override
 	public String play() {
 		return "bassoon"; 
 	}
 	
+        @Override
 	public int getSoundVolume() {
 		return 9001;
 	}
 	
-	public String color() {
+        @Override
+	public String getColor() {
 		return "red but a little purple too";
-	}
+        }
 }
