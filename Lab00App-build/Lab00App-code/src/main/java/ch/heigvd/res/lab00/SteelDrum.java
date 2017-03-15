6130d34b0d65957f2e6d7c42d1d8427df5035d7c
silver kameni <silverkameni@yahoo.fr>
2017-03-15 16:26:16 +0100

    fixes #420

--- Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/SteelDrum.java
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/SteelDrum.java
@@ -1,11 +1,5 @@
 package ch.heigvd.res.lab00;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author lognaume
